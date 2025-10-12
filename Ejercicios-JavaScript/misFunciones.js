df3421adbe0a01cdf7cf01f093be7588f47b4fcc
marcoshernandez1 <2424488@ucc.edu.ar>
2025-10-12 15:28:18 -0300

    ejercicio Conversión de Unidades refactorizado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -7,6 +7,44 @@
  * @return
  */
 
+convertirUnidades = (id,valor) => {
+    let met, pul, pie, yar;
+
+    if(valor.includes(",")){
+        valor = valor.replace(",", ".")
+    }
+    if(isNaN(valor)){
+        alert("El valor ingresado es incorrecto");
+        met = "";
+        pul = "";
+        pie = "";
+        yar = "";
+    }else if(id==="metro"){
+        met = valor;
+        pul = valor*39.3701;
+        pie = valor*3.28084;
+        yar = valor*1.09361;
+    }else if(id==="pulgada"){
+        met= 0.0254 * valor;
+    pie = 0.083333 * valor;
+    yar = 0.02777 * valor;
+    }
+    else if(id==="pie"){
+      pul = 12 * valor;
+   met = 0.304 * valor;
+    yar= 0.33333 * valor;  
+
+    }else if(id==="yarda"){
+       pul = 36 * valor;
+   pie = 3 * valor;
+    met = 0.9144 * valor;
+    }
+    document.lasunidades.unid_metro.value = Math.round(met*100)/100;
+    document.lasunidades.unid_pulgada.value = Math.round(pul*100)/100;
+    document.lasunidades.unid_pie.value = Math.round(pie);
+    document.lasunidades.unid_yarda.value = Math.round(yar);
+}
+
 function cambiarunidades(id, value) {
     
 let valor = parseFloat(value);
